4a130ed7d467859435953cd051acec55669d6a85
NickBowley <116365231+NickBowley@users.noreply.github.com>
2024-04-23 16:31:07 +0100

    added index stuff

--- students.php
+++ students.php
@@ -1,5 +1,19 @@
 <?php 
 
+
+   include("_includes/config.inc");
+   include("_includes/dbconnect.inc");
+   include("_includes/functions.inc");
+
+   echo template("templates/partials/header.php");
+
+   if (isset($_GET['return'])) {
+      $msg = "";
+      if ($_GET['return'] == "fail") {$msg = "Login Failed. Please try again.";}
+      $data['message'] = "<p>$msg</p>";
+    }
+
+
 $servername = "localhost";
 $username = "";
 $password = "";
