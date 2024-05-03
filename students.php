364c6df4849163f6a865ac1eaf14a73ee08ff3a0
NickBowley <116365231+NickBowley@users.noreply.github.com>
2024-05-03 12:29:24 +0100

    trying again

--- students.php
+++ students.php
@@ -1,21 +1,55 @@
 <?php
 
-include("_includes/config.inc");
-include("_includes/dbconnect.inc");
-include("_includes/functions.inc");
-require_once 'vendor/autoload.php';
+   include("_includes/config.inc");
+   include("_includes/dbconnect.inc");
+   include("_includes/functions.inc");
 
-// use the factory to create a Faker\Generator instance
-$faker = Faker\Factory::create();
+   
+   echo template("templates/partials/header.php");
+   
+   //faker library
+   require_once 'vendor/autoload.php';
 
-if (isset($_SESSION['id'])) {
+   //declaring new instance of Faker Library
+   $faker = Faker\Factory::create();
 
-    $dob = $faker->dateTimeThisCentury->format('Y-m-d');
+   //db connection
+   $db = new mysqli("localhost", "root", "", "oss-cw2");
 
-    $sql = "INSERT INTO student (studentid, dob, firstname, lastname, house, town, county)
-            VALUES ('" . $_POST['studentid'] . "','" . $dob . "','" . $faker->firstname . "','" . $faker->lastname . "','" . $faker->house. "','" . $faker->town . "','" . $faker->county . "','";
+   //inserting manually
+   
+   //$db->query("INSERT INTO student(studentid, password, dob, firstname, lastname, house, town, county, country, postcode) 
+   //VALUES ('2000000','$2y$10$.LJBO164nZWEVVE/v5mgNuzR01zx1zoyXuGJUa/zp2U.MQxkps3LS','1974-11-10','Jon','Smith','23 Victoria Road',
+   //'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');");
+
+   // Insert 5 student records into the database using faker library
+  
+   for ($i = 0; $i < 5; $i++) {
+      $studentid = $faker->unique()->numberBetween(20000001, 29999999);
+      $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+     // $password = $faker->password();
+      $dob = mysqli_real_escape_string($db, $faker->date());
+      $firstname = mysqli_real_escape_string($db, $faker->firstName());
+      $lastname = mysqli_real_escape_string($db, $faker->lastName());
+      $house = mysqli_real_escape_string($db, $faker->buildingNumber());
+      $town = mysqli_real_escape_string($db, $faker->city());
+      $county = mysqli_real_escape_string($db, $faker->state());
+      $country = mysqli_real_escape_string($db, $faker->country());
+      $postcode = mysqli_real_escape_string($db, $faker->postcode());
       
-    $result = mysqli_query($conn,$sql);
-}
+      
+
+  
+      $sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
+              VALUES ('$studentid', '$hashed_password', '$dob', '$firstname', '$lastname', '$house', '$town', '$county', '$country',
+               '$postcode')";
+
+      $db->query($sql);
+  }
+
+  // if my query is correct this message should be displayed:
+  echo "Inserted 5 student records into the database.";
+
+  echo template("templates/partials/footer.php");
 
 ?>
