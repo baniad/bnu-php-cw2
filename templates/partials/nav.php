897726aa6335a3c80aa122821bf6e6a4c89afde5
NickBowley <116365231+NickBowley@users.noreply.github.com>
2024-05-10 15:44:20 +0100

    bootstarp added to nav

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -1,23 +1,30 @@
-
-<div id="main-nav">
-<ul class="nav nav-pills">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="modules.php">My Modules</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link"  href="students.php">Students</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="assignmodule.php">Assign Module</a>
-  </li>
-    <li class="nav-item">
-    <a class="nav-link"  href="details.php">My Details</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="addstudent.php">Add Student</a>
-  </li>
-    <li class="nav-item">
-    <a class="nav-link" href="logout.php">Logout</a>
-  </li>
-</ul>
-</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Brand Name</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="modules.php">My Modules</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="students.php">Students</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="assignmodule.php">Assign Module</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="details.php">My Details</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="addstudent.php">Add Student</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
