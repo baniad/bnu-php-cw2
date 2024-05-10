de76159f41eb86d3cc431a1a3ac74686c1ecb3ce
NickBowley <116365231+NickBowley@users.noreply.github.com>
2024-05-10 11:57:04 +0100

    attempt

--- students.php
+++ students.php
@@ -1,47 +1,27 @@
 <?php
 
-include("_includes/config.inc");
+   include("_includes/config.inc");
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
 
-   echo template("templates/partials/header.php");
+   // when installed via composer
+    require_once 'vendor/autoload.php';
 
-require_once 'vendor/autoload.php';
+    require_once 'vendor/autoload.php';
 
-// Create a new instance of Faker
+// use the factory to create a Faker\Generator instance
 $faker = Faker\Factory::create();
 
-// Connect to the database
-$db = new mysqli("localhost", "root", "", "php_cw2");
 
-// Check the connection
-if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
-}
+  for ($i=0; $i < 5; $i++){
+      $sql = "INSERT  INTO student
+            (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
+            VALUES ('$i', '{$faker->password()}', '{$faker->date('Y_m_d')}','{$faker->firstname()}','{$faker->lastname()}', '{$faker->secondaryAddress()}', '{$faker->citySuffix()}',
+                        '{$faker->state()}', '{$faker->country()}', '{$faker->postcode()}')";
+                       
+                        
+      $result = mysqli_query($conn,$sql);
+               
+  }
 
-// Insert 5 student records into the database
-for ($i = 0; $i < 5; $i++) {
-    $studentid = $faker->unique()->numberBetween(20000001, 29999999);
-    $password = password_hash($faker->password, PASSWORD_DEFAULT);
-    $dob = $faker->date('Y-m-d');
-    $firstname = $db->real_escape_string($faker->firstName);
-    $lastname = $db->real_escape_string($faker->lastName);
-    $house = $db->real_escape_string($faker->buildingNumber);
-    $town = $db->real_escape_string($faker->city);
-    $county = $db->real_escape_string($faker->state);
-    $country = $db->real_escape_string($faker->country);
-    $postcode = $db->real_escape_string($faker->postcode);
-
-    $sql = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
-            VALUES ('$studentid', '$password', '$dob', '$firstname', '$lastname', '$house', '$town', '$county', '$country', '$postcode')";
-
-    if ($db->query($sql) === TRUE) {
-        echo "New record created successfully\n";
-    } else {
-        echo "Error: " . $sql . "\n" . $db->error;
-    }
-}
-    echo template("templates/partials/footer.php");
-
-    $db->query($sql);
-
+?>
