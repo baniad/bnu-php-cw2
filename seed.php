655ae39208b7b2018c30d3901ff07d0e58f52b0d
NickBowley <116365231+NickBowley@users.noreply.github.com>
2024-05-10 12:25:37 +0100

    sybntax error

--- seed.php
+++ seed.php
@@ -12,14 +12,23 @@
 // use the factory to create a Faker\Generator instance
 $faker = Faker\Factory::create();
 
-
 for ($i=0; $i < 5; $i++){
     $studentid = $faker->unique()->numberBetween(20000001, 29999999);
-    $sql = "INSERT  INTO student
+    $password = mysqli_real_escape_string($conn, $faker->password());
+    $dob = $faker->date('Y_m_d');
+    $firstname = mysqli_real_escape_string($conn, $faker->firstname());
+    $lastname = mysqli_real_escape_string($conn, $faker->lastname());
+    $house = mysqli_real_escape_string($conn, $faker->secondaryAddress());
+    $town = mysqli_real_escape_string($conn, $faker->citySuffix());
+    $county = mysqli_real_escape_string($conn, $faker->state());
+    $country = mysqli_real_escape_string($conn, $faker->country());
+    $postcode = mysqli_real_escape_string($conn, $faker->postcode());
+
+    $sql = "INSERT INTO student
         (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
-        VALUES ('$studentid', '{$faker->password()}', '{$faker->date('Y_m_d')}','{$faker->firstname()}','{$faker->lastname()}', '{$faker->secondaryAddress()}', '{$faker->citySuffix()}',
-                    '{$faker->state()}', '{$faker->country()}', '{$faker->postcode()}')";
+        VALUES ('$studentid', '$password', '$dob', '$firstname', '$lastname', '$house', '$town', '$county', '$country', '$postcode')";
     $result = mysqli_query($conn,$sql);
 }
+?>
 ?>
 
