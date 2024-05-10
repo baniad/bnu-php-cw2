e087448b86c9a50d032d5459984eea1f5b5e94e7
NickBowley <116365231+NickBowley@users.noreply.github.com>
2024-05-10 12:23:02 +0100

    changed the faker

--- seed.php
+++ seed.php
@@ -13,15 +13,14 @@
 $faker = Faker\Factory::create();
 
 
-  for ($i=0; $i < 5; $i++){
-      $sql = "INSERT  INTO student
-            (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
-            VALUES ('$i', '{$faker->password()}', '{$faker->date('Y_m_d')}','{$faker->firstname()}','{$faker->lastname()}', '{$faker->secondaryAddress()}', '{$faker->citySuffix()}',
-                        '{$faker->state()}', '{$faker->country()}', '{$faker->postcode()}')";
-                       
-                        
-      $result = mysqli_query($conn,$sql);
-               
-  }
-
+<?php
+for ($i=0; $i < 5; $i++){
+    $studentid = $faker->unique()->numberBetween(20000001, 29999999);
+    $sql = "INSERT  INTO student
+        (studentid, password, dob, firstname, lastname, house, town, county, country, postcode)
+        VALUES ('$studentid', '{$faker->password()}', '{$faker->date('Y_m_d')}','{$faker->firstname()}','{$faker->lastname()}', '{$faker->secondaryAddress()}', '{$faker->citySuffix()}',
+                    '{$faker->state()}', '{$faker->country()}', '{$faker->postcode()}')";
+    $result = mysqli_query($conn,$sql);
+}
 ?>
+
