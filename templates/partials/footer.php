c97bb1c9ea142358174147d017935542e14fb413
NickBowley <116365231+NickBowley@users.noreply.github.com>
2024-05-10 12:19:03 +0100

    footer

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,7 +1,8 @@
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" 
-crossorigin="anonymous"></script>
-   </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+</body>
 
 </html>
