e1180166ff5f8046af15bc98e08f4c55e0e62002
NickBowley <116365231+NickBowley@users.noreply.github.com>
2024-05-17 13:14:32 +0100

    changed the layout

--- students.php
+++ students.php
@@ -1,66 +1,47 @@
 <?php
 
-   include("_includes/config.inc");
-   include("_includes/dbconnect.inc");
-   include("_includes/functions.inc");
+include("_includes/config.inc");
+include("_includes/dbconnect.inc");
+include("_includes/functions.inc");
 
-   // check logged in
-   if (isset($_SESSION['id'])) {
+// check logged in
+if (isset($_SESSION['id'])) {
 
-      echo template("templates/partials/header.php");
-      echo template("templates/partials/nav.php");
+   echo template("templates/partials/header.php");
+   echo template("templates/partials/nav.php");
 
-      $sql = "SELECT * FROM student";
+   $sql = "SELECT * FROM student";
 
-      $result = mysqli_query($conn,$sql);
+   $result = mysqli_query($conn,$sql);
 
-      $data['content'] .= '<form onsubmit="return confirm(\'Are you sure you want to delete?\')" action="deletestudents.php" method="POST">';
-      
-      // prepare page content
-      $data['content'] .= "<table class='table table-striped'>";
-      $data['content'] .= "<thead class='thead-dark'>
-                             <tr>
-                               <th>Student ID</th>
-                               <th>DOB</th>
-                               <th>First Name</th>
-                               <th>Last Name</th>
-                               <th>House</th>
-                               <th>Town</th>
-                               <th>County</th>
-                               <th>Country</th>
-                               <th>Postcode</th>
-                               <th>File</th>
-                             </tr>
-                           </thead>";
-      
-      // Display the student information within the html table
-      while($row = mysqli_fetch_array($result)) {
-         $data['content'] .= "<tr>";
-         $data['content'] .= "<td> {$row["studentid"]} </td>";
-         $data['content'] .= "<td> {$row["dob"]}</td>";
-         $data['content'] .= "<td> {$row["firstname"]}</td>";
-         $data['content'] .= "<td> {$row["lastname"]}</td>";
-         $data['content'] .= "<td> {$row["house"]} </td>";
-         $data['content'] .= "<td> {$row["town"]} </td>";
-         $data['content'] .= "<td> {$row["county"]} </td>";
-         $data['content'] .= "<td> {$row["country"]} </td>";
-         $data['content'] .= "<td> {$row["postcode"]} </td>";
-         $data['content'] .= "<td> {$row["imagedata"]} </td>";
-         $data['content'] .= "<td> <input type='checkbox' name='students[]' value='$row[studentid]'/></td>";
-         $data['content'] .= "</tr>";
-      }
-      $data['content'] .= "</table>";
-      $data['content'] .= "<input type='submit' name='deletebtn' value='Delete'/>";
-      $data['content'] .= "</form>";
-      
-      // render the template
-      echo template("templates/default.php", $data);
-     }
-     else 
-     {
-      header("Location: index.php");
-     }
+   $data['content'] .= '<form onsubmit="return confirm(\'Are you sure you want to delete?\')" action="deletestudents.php" method="POST">';
+   
+   // Display the student information within Bootstrap cards
+   while($row = mysqli_fetch_array($result)) {
+      $data['content'] .= "<div class='card' style='width: 18rem;'>";
+      $data['content'] .= "<div class='card-body'>";
+      $data['content'] .= "<h5 class='card-title'>{$row["firstname"]} {$row["lastname"]}</h5>";
+      $data['content'] .= "<p class='card-text'>DOB: {$row["dob"]}</p>";
+      $data['content'] .= "<p class='card-text'>House: {$row["house"]}</p>";
+      $data['content'] .= "<p class='card-text'>Town: {$row["town"]}</p>";
+      $data['content'] .= "<p class='card-text'>County: {$row["county"]}</p>";
+      $data['content'] .= "<p class='card-text'>Country: {$row["country"]}</p>";
+      $data['content'] .= "<p class='card-text'>Postcode: {$row["postcode"]}</p>";
+      $data['content'] .= "<input type='checkbox' name='students[]' value='{$row["studentid"]}'/>";
+      $data['content'] .= "</div>";
+      $data['content'] .= "</div>";
+   }
+   $data['content'] .= "<input type='submit' name='deletebtn' value='Delete'/>";
+   $data['content'] .= "</form>";
+   
+   // render the template
+   echo template("templates/default.php", $data);
+}
+else 
+{
+   header("Location: index.php");
+}
 
-   echo template("templates/partials/footer.php");
+echo template("templates/partials/footer.php");
 
 ?>
