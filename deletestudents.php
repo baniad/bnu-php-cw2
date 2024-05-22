772d26a4a92a7c95cd93cffe2e4a84d1b2271269
NickBowley <116365231+NickBowley@users.noreply.github.com>
2024-05-22 11:24:55 +0100

    added confirm delete

--- deletestudents.php
+++ deletestudents.php
@@ -1,32 +1,26 @@
 <?php
-
    include("_includes/config.inc");
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
 
-
    // check logged in
    if (isset($_SESSION['id'])) {
+       if (empty($_POST['students'])){
+           header("Location: students.php");
+       }
 
-   
-  
-
-   if (empty($_POST['students'])){
-
-       header("Location: students.php");
+       foreach($_POST['students'] as $theStudent){
+           echo "<script>
+               var r = confirm('Are you sure you want to delete this student?');
+               if (r == true) {
+                   <?php
+                       $sql = 'DELETE FROM student WHERE studentid = ' . $theStudent;
+                       $result = mysqli_query($conn,$sql);
+                   ?>
+               } else {
+                   // User pressed cancel, do nothing
+               }
+           </script>";
+       }
    }
-
-   foreach($_POST['students'] as $theStudent){
-        $sql = "DELETE FROM student WHERE studentid = $theStudent";
-
-        $result = mysqli_query($conn,$sql);
-   }
-
-
-   header("Location: students.php");
-
-   } else {
-      header("Location: index.php");
-   }
-
 ?>
